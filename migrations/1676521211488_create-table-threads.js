85f1640b3854ce204369c7dfe070617be4ae2d35
sarahzemo <sarah.zemo33@gmail.com>
2023-03-13 17:29:35 +0700

    add constraint db

--- migrations/1676521211488_create-table-threads.js
+++ migrations/1676521211488_create-table-threads.js
@@ -24,7 +24,7 @@ exports.up = pgm => {
         }
     });
 
-    // pgm.addConstraint('threads', 'fk_threads.owner_users.id', 'FOREIGN KEY(owner) REFERENCES users(id) ON UPDATE CASCADE');
+    pgm.addConstraint('threads', 'fk_threads.owner_users.id', 'FOREIGN KEY(owner) REFERENCES users(id) ON UPDATE CASCADE');
 }
 exports.down = (pgm) => {
     pgm.dropTable('threads');
